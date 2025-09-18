4db59df6891494a6296c0b110d0e64f8d204ccb4
Giovanna Lacerda <seuemail@exemplo.com>
2025-09-18 15:43:20 -0300

    Adicionando todos os arquivos do projeto

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import Mensagem from './components/Mensagem.vue';
+
+export default{
+components: {Mensagem}
+} 
+</script>
+
+<template>
+  <Mensagem mensagem="Olá Mundo!"/>
+</template>
---
+++ src/components/Mensagem.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+export default{
+  props: {
+    mensagem:{
+      type: String
+    }
+  }
+}
+</script>
+<template>{{ mensagem }}</template>
